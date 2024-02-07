8ed854e9f73bc8e4da720b9fdf93692306ab1356
Eduardo Martinez <eduardo_martinez87@hotmail.com>
2024-02-06 21:01:12 -0600

    Variables de entorno agregadas y configuradas

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const dbConnection = async () => {
   try {
-    const db = await mongoose.connect("mongodb+srv://eduardomartinez87:kkzWWvZrHH8AiOhy@cluster0.22skw8o.mongodb.net/?retryWrites=true&w=majority", {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import dbConnection from './config/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+
 dbConnection();
+
 
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
